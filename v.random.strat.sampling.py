5be42dc955380e868474a767432bb2e7b4b96e0c
Anika Bettge <bettge@mundialis.de>
2019-12-04 12:33:32 +0000

    Apply suggestion to actinia_deploy_actinia/actinia-core-dev-s-incora/plugins/v.random.strat.sampling/v.random.strat.sampling.html

--- v.random.strat.sampling.py
+++ v.random.strat.sampling.py
@@ -3,75 +3,91 @@
 ############################################################################
 #
 # MODULE:      v.random.strat.sampling
-# AUTHOR(S):   Hajar Benelcadi, <benelcadi at mundialis.de>
+# AUTHOR(S):   Hajar Benelcadi and Anika Bettge
 #
 # PURPOSE:     sampling polygon vector data in another points vector data based on random stratified sampling
-# COPYRIGHT:   (C) 2019 by Anika Bettge, mundialis
+# COPYRIGHT:   (C) 2019 by Hajar Benelcadi and Anika Bettge, mundialis
 #
 #              This program is free software under the GNU General Public
 #              License (>=v2). Read the file COPYING that comes with GRASS
 #              for details.
 #
 #############################################################################
-#%Module
+#%module
 #% description: sample data
-#% keyword: display
-#% keyword: raster
-#%End
+#% keyword: vector
+#% keyword: sampling
+#% keyword: statistics
+#% keyword: random
+#% keyword: point pattern
+#% keyword: stratified random sampling
+#%end
 
+#%option G_OPT_V_INPUT
+#% description: Name of input vector map
+#% required : yes
+#%end
+
+#%option G_OPT_DB_COLUMN
+#% key: column
+#% description: Name of column with class information
+#% required : yes
+#%end
+
+#%option G_OPT_V_OUTPUT
+#% key: output
+#% description: Name of output vector map
+#% required : yes
+#%end
+
+#%option
+#% key: npoints
+#% type: integer
+#% required: yes
+#% description: Number of points to be created for each class
+#% answer: 100
+#%end
 
 from grass.script import core as grass
+import os
+
+
+def cleanup(rm_vectors):
+    grass.message(_("Cleaning up..."))
+    nuldev = open(os.devnull, 'w')
+    for rm_v in rm_vectors:
+        grass.run_command(
+            'g.remove', flags='f', type='vector', name=rm_v, quiet=True, stderr=nuldev)
+
 
 def main():
-```
-# import vector data in grass
-v.in.ogr in=/home/hbenelcadi/geodata_dav/projekte/incora_test_Dortmund/auxilliary_data/training_data_ILS/incora_dortmund_LULC_training_merged_EPSG32632.gpkg out=d_p_36236 --o
+    input = options['input']
+    output = options['output']
+    column = options['column']
+    npoints = options['npoints']
 
-# reproject data if not in the correct projection (in this case it was already done)
-v.proj in=d_p_36236 location=Dortmund_UTM32N mapset=PERMANENT
+    # sampling
+    grass.message("Sampling for each class separately...")
+    classes = grass.parse_command(
+        'v.db.select', map=input,
+        column=column, flags='c')
 
-# visualise vector table
-v.db.select map=d_p_36236
+    class_outputs = []
+    for cl in classes:
+        random_output = 'v_random_strat_sampling_%s_%s' % (cl, str(os.getpid()))
+        where_str = "%s = '%s'" % (column, cl)
+        grass.run_command(
+            'v.random', restrict=input, where=where_str,
+            output=random_output, npoints=npoints)
+        class_outputs.append(random_output)
 
-# sampling
-v.random restrict=d_p_36236 output=d_p_36236_tree_shrubs npoints=500 where="lulc = 'tree_shrubs'" --o
-v.random restrict=d_p_36236 output=d_p_36236_water npoints=500 where="lulc = 'water'" --o
-v.random restrict=d_p_36236 output=d_p_36236_non_tree_vegetation npoints=500 where="lulc = 'non_tree_vegetation'" --o
-v.random restrict=d_p_36236 output=d_p_36236_built_up npoints=500 where="lulc = 'built_up'" --o
+    # combine vector data
+    grass.message("Compining sampled vector data...")
+    grass.run_command(
+        'v.patch', flags='e', input=','.join(class_outputs), output=output)
 
-# combine vector data
-v.patch -e in=d_p_36236_tree_shrubs,d_p_36236_water,d_p_36236_non_tree_vegetation,d_p_36236_built_up out=d_p_36236_sampled_all
-
-# export in GPKG
-v.out.ogr -s input=d_p_36236_sampled_all type=point output=/home/hbenelcadi/geodata_dav/projekte/incora_test_Dortmund/auxilliary_data/training_data_ILS/incora_dortmund_LULC_training_merged_sampled_all_EPSG36236.gpkg --o
-```
-
-    vectorlist = grass.parse_command('g.list', type='vector', pattern='*B*_10m')
-    for rast_mapset in rasterlist_10m:
-        rast = rast_mapset.split('@')[0]
-        grass.run_command('g.rename', raster=rast + ',' + rast[-7:-4])
-
-    rasterlist_20m = grass.parse_command('g.list', type='raster', pattern='*B*_20m')
-    for rast_mapset in rasterlist_20m:
-        rast = rast_mapset.split('@')[0]
-        r_10m = rast[:-3] + '10m'
-        if not r_10m in rasterlist_10m:
-            grass.run_command('g.rename', raster=rast + ',' + rast[-7:-4])
-        else:
-            grass.run_command('g.remove', type='raster', name=rast, flags='f')
-
-    rasterlist_60m = grass.parse_command('g.list', type='raster', pattern='*B*_60m')
-    for rast_mapset in rasterlist_60m:
-        rast = rast_mapset.split('@')[0]
-        r_10m = rast[:-3] + '10m'
-        r_20m = rast[:-3] + '20m'
-        if not r_10m in rasterlist_10m and not r_20m in rasterlist_20m:
-            grass.run_command('g.rename', raster=rast + ',' + rast[-7:-4])
-        else:
-            grass.run_command('g.remove', type='raster', name=rast, flags='f')
-
-
-    grass.message('sampling DONE.')
+    cleanup(class_outputs)
+    grass.message("Sampling DONE <%s>" % output)
 
 
 if __name__ == "__main__":
